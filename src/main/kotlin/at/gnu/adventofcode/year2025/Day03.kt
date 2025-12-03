95986e0354056b7e4120bba7c488919be598707c
gnu11111 <gnu@gnu.at>
2025-12-03 13:45:37 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2025/Day03.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2025/Day03.kt
@@ -2,44 +2,21 @@ package at.gnu.adventofcode.year2025
 
 class Day03(val banks: List<String>) {
 
-    fun part1(): Long {
-        var jolts = 0L
-        for (bank in banks) {
-            val sorted = bank.toCharArray().sorted().reversed()
-            val largest = sorted.first().toString()
-            val largestIndex = bank.indexOf(largest)
-            val rest = bank.substring(largestIndex + 1)
-            val restSorted = rest.toCharArray().sorted().reversed()
-            val largestFromRest = if (restSorted.isNotEmpty()) restSorted.first().toString() else 0
-            val secondLargest = sorted.drop(1).first().toString()
-            val secondLargestIndex = bank.indexOf(secondLargest)
-            val plus = when {
-                largest == secondLargest -> (largest + secondLargest).toInt()
-                largestIndex == (bank.length - 1) -> (secondLargest + largest).toInt()
-                largestIndex > secondLargestIndex -> (largest + largestFromRest).toInt()
-                else -> (largest + secondLargest).toInt()
-            }
-            jolts += plus
-        }
-        return jolts
-    }
+    fun part1(): Long =
+        calculateJoltage()
 
-    fun part2(): Long {
-        var totalOutputJoltage = 0L
-        for (bank in banks) {
-            var batteries = bank
-            var joltage = 0L
-            for (i in 12 downTo 1) {
-                val batteriesToConsider = batteries.dropLast(i - 1)
-                val largestBattery = batteriesToConsider.toCharArray().sorted().reversed().first()
-                val largestBatteryIndex = batteriesToConsider.indexOf(largestBattery)
-                joltage = (10L * joltage) + largestBattery.digitToInt()
-                batteries = batteries.substring(largestBatteryIndex + 1)
-            }
-            totalOutputJoltage += joltage
+    fun part2(): Long =
+        calculateJoltage(digits = 12)
+
+
+    private fun calculateJoltage(digits: Int = 2) =
+        banks.sumOf { bank ->
+            (digits downTo 1).fold(0L to 0) { (joltage, firstIndex), digit ->
+                val batteriesToConsider = bank.substring(firstIndex, bank.length - digit + 1)
+                val (batteryIndex, battery) = batteriesToConsider.withIndex().maxBy { it.value }
+                ((10L * joltage) + battery.digitToInt()) to (firstIndex + batteryIndex + 1)
+            }.first
         }
-        return totalOutputJoltage
-    }
 
     companion object {
         const val RESOURCE = "/adventofcode/year2025/Day03.txt"
