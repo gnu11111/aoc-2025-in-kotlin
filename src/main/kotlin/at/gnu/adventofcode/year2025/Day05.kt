4327ac9eaadb9b105830be97251357d05562c441
gnu11111 <gnu@gnu.at>
2025-12-05 13:12:32 +0100

    Day 05 - Cafeteria

---
+++ src/main/kotlin/at/gnu/adventofcode/year2025/Day05.kt
@@ -0,0 +1,56 @@
+package at.gnu.adventofcode.year2025
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+class Day05(freshIngridients: List<String>, private val ingridients: List<Long>) {
+
+    private val freshIngridients = freshIngridients.map {
+        val (start, end) = it.split("-")
+        start.toLong()..end.toLong()
+    }
+
+
+    fun part1(): Long =
+        ingridients.count { ingredient -> freshIngridients.any { ingredient in it } }.toLong()
+
+    fun part2(): Long {
+        val ranges = freshIngridients.toMutableSet()
+        val add = mutableSetOf<LongRange>()
+        val remove = freshIngridients.toMutableSet()
+        while (remove.isNotEmpty()) {
+            add.clear()
+            remove.clear()
+            for (range in ranges) {
+                val first = ranges.firstOrNull { (range != it) && (range.first in it) }
+                if (first != null) {
+                    add += first.first..first.last.coerceAtLeast(range.last)
+                    remove += range
+                }
+                val last = ranges.firstOrNull { (range != it) && (range.last in it) }
+                if (last != null) {
+                    add += range.first.coerceAtMost(last.first)..last.last
+                    remove += range
+                }
+            }
+            remove.removeAll(add)
+            ranges.removeAll(remove)
+            ranges.addAll(add)
+        }
+        return ranges.sumOf { it.last - it.first + 1L }
+    }
+
+    companion object {
+        const val RESOURCE = "/adventofcode/year2025/Day05.txt"
+    }
+}
+
+@ExperimentalTime
+fun main() {
+    val input = Day05::class.java.getResource(Day05.RESOURCE)!!.readText().trim().split("\n\n", "\r\n\r\n")
+    val freshIngridients = input.first().trim().split("\n", "\r\n")
+    val ingridients = input.last().trim().split("\n", "\r\n").map(String::toLong)
+    val day05 = Day05(freshIngridients, ingridients)
+    measureTimedValue { day05.part1() }.let { (result, time) -> println("Day05::part1 -> $result [$time]") }
+    measureTimedValue { day05.part2() }.let { (result, time) -> println("Day05::part2 -> $result [$time]") }
+}
---
+++ src/test/kotlin/at/gnu/adventofcode/year2025/Day05Test.kt
@@ -0,0 +1,29 @@
+package at.gnu.adventofcode.year2025
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+class Day05Test {
+
+    private val freshIngredients = listOf("3-5", "10-14", "16-20", "12-18")
+    private val ingredients = listOf(1L, 5L, 8L, 11L, 17L, 32L)
+
+    private val test = mapOf(
+        Day05::part1 to 3L,
+        Day05::part2 to 14L
+    )
+
+    @Test @ExperimentalTime
+    fun testMySolution() {
+        for (function in test.keys) {
+            val (result, time) = measureTimedValue {
+                function.invoke(Day05(freshIngredients, ingredients))
+            }
+            println("Day05::${function.name}: ${freshIngredients.size} fresh "
+                + "ingredients ranges, ${ingredients.size} ingredients -> $result [${time}]")
+            assertEquals(test[function], result)
+        }
+    }
+}
